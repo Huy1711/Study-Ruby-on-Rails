e974af2c5c36d5be645a4eba0393085b19edd4ea
huy.ng <nguyenduchuy1711@gmail.com>
2024-10-25 22:16:50 +0700

    exercise 2,3: add helper test, sign up page test

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -9,4 +9,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")
+  end
 end
